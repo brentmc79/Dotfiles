37b9a5b95275da67f5829bfbfbe33197a477515f
Brent Collier <brentmc79@gmail.com>
2025-12-27 10:36:20 -0800

    Add Zsh default shell setting to setup.sh
    The setup.sh script now checks if 'zsh' is present in the Brewfile.
    If 'zsh' is found, it will attempt to set Zsh as the default shell
    using OS-specific commands (dscl for macOS, chsh for Linux/Fedora).
    A message is added to inform the user to restart the terminal for changes to take effect.

--- setup.sh
+++ setup.sh
@@ -3,15 +3,6 @@
 # Exit immediately if a command exits with a non-zero status.
 set -e
 
-# --- Oh My Zsh ---
-if [ -d "$HOME/.oh-my-zsh" ]; then
-  echo "Oh My Zsh is already installed. Updating..."
-  sh $HOME/.oh-my-zsh/tools/upgrade.sh
-else
-  echo "Installing Oh My Zsh..."
-  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-fi
-
 # --- Homebrew ---
 if command -v brew &>/dev/null; then
   echo "Homebrew is already installed. Updating..."
@@ -29,8 +20,39 @@ else
   echo "Brewfile not found. Skipping bundle installation."
 fi
 
-# --- Symlink Dotfiles ---
-echo "Symlinking dotfiles..."
+# --- Oh My Zsh ---
+if [ -d "$HOME/.oh-my-zsh" ]; then
+  echo "Oh My Zsh is already installed. Updating..."
+  sh $HOME/.oh-my-zsh/tools/upgrade.sh
+else
+  echo "Installing Oh My Zsh..."
+  sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+fi
+
+# --- Set Zsh as default shell if specified in Brewfile ---
+if grep -q "brew \"zsh\"" Brewfile; then
+  echo "Zsh found in Brewfile. Checking if it's the default shell."
+  if [ "$SHELL" != "$(which zsh)" ]; then
+    echo "Setting Zsh as the default shell..."
+    if [[ "$(uname)" == "Darwin" ]]; then
+      # macOS
+      current_shell=$(dscl . -read /Users/$USER UserShell | awk '{print $2}')
+      sudo dscl . -change /Users/$USER UserShell "$current_shell" /bin/zsh
+    elif [[ "$(uname)" == "Linux" ]]; then
+      # Linux (e.g., Fedora)
+      chsh -s "$(which zsh)"
+    else
+      echo "Unsupported OS for automatic Zsh default shell setting."
+    fi
+  else
+    echo "Zsh is already the default shell."
+  fi
+    else
+      echo "Zsh not found in Brewfile. Skipping default shell setting."
+    fi
+    echo "Please restart your terminal or log out and back in for the default shell change to take effect."
+
+# --- Symlink Dotfiles ---echo "Symlinking dotfiles..."
 for file in .*; do
   case $file in
     .|..|.git|.gitignore)
